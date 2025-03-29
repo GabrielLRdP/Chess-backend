783a4993594e820b12b3996a473c13e64c5ee916
GABRIEL <gleroy@acheel.com>
2025-03-29 16:42:48 +0100

    users room setup

--- src/infrastructure/socket/socketServer.ts
+++ src/infrastructure/socket/socketServer.ts
@@ -11,13 +11,33 @@ export const initSocket = (server: HttpServer) => {
   io.on('connection', (socket) => {
     const token = socket.handshake.auth?.token;
     const decoded = jwt.decode(token) as UserPayload;
-    console.log(token);
-    console.log(decoded);
-    console.log(userMap);
     const socketId = socket.id;
-    console.log(`User connected - ${socketId}`);
+    console.log(`user connected - ${socketId} \n user infos :${decoded}`);
+    userMap.set(socketId, decoded);
+    console.log('userMap: ', userMap);
+
+    io.to('online-users-room').emit(
+      'update-users-list',
+      Array.from(userMap.values())
+    );
+    socket.on('join-users-room', (userData) => {
+      socket.join('online-users-room');
+      console.log('room joined AAAAAAAA');
+      io.to(socket.id).emit('update-users-list', Array.from(userMap.values()));
+    });
+
+    socket.on('leave-users-room', () => {
+      socket.leave('online-usesr-room');
+    });
+
     socket.on('disconnect', (socket) => {
+      userMap.delete(socketId);
       console.log(`User disconnected - ${socketId}`);
+      console.log('userMap: ', userMap);
+      io.to('online-users-room').emit(
+        'update-users-list',
+        Array.from(userMap.values())
+      );
     });
   });
 
