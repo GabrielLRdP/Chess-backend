54bd86470611ea64376bf1cb2cd33c814d363391
GABRIEL <gleroy@acheel.com>
2025-04-26 12:11:00 +0200

    feat: handling positive response, setting game-started event

--- src/api/socket/controllers/respondToInvitation.ts
+++ src/api/socket/controllers/respondToInvitation.ts
@@ -19,6 +19,28 @@ export const respondToInvitation = (
     from: { userName: sender?.userName, userId: sender?.userId },
     accepted: data.accepted,
   });
+
+  if (data.accepted && sender && receiver) {
+    const roomId = `game-${[sender.userId, receiver.userId].sort().join('-')}`;
+    const randomColor = Math.random() > 0.5 ? 'white' : 'black';
+    socket.join(roomId);
+    io.sockets.sockets.get(targetSocketId)?.join(roomId);
+    io.to(roomId).emit('game-started', {
+      roomId,
+      players: [
+        {
+          userId: sender.userId,
+          userName: sender.userName,
+          color: randomColor,
+        },
+        {
+          userId: receiver.userId,
+          userName: receiver.userName,
+          color: randomColor === 'white' ? 'black' : 'white',
+        },
+      ],
+    });
+  }
 };
 
 type InvitationResponse = {
