4c2bc05c3b5ed466087884289b69149f4f9a1553
GABRIEL <gleroy@acheel.com>
2025-04-29 00:34:19 +0200

    fix: promotion when online

--- src/api/socket/controllers/makeMove.ts
+++ src/api/socket/controllers/makeMove.ts
@@ -1,13 +1,14 @@
 import { Socket } from 'socket.io';
 
 export const makeMove = (socket: Socket, data: any) => {
-  const { roomId, previousPiecePosition, newPiecePosition } = data;
+  const { roomId, previousPiecePosition, newPiecePosition, promotion } = data;
   if (!roomId) {
     console.error('Invalid make-move payload', data);
     return;
   }
   socket.to(roomId).emit('opponent-move', {
-    previousPiecePosition,
-    newPiecePosition,
+    previousPiecePosition: previousPiecePosition,
+    newPiecePosition: newPiecePosition,
+    promotion: promotion,
   });
 };
